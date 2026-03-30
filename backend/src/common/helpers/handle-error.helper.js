4a164be7e0bd1e9a427a0040cff7de10c3685235
itBaowy <notbao.js@gmail.com>
2026-03-31 00:38:45 +0700

    jsonwebtoken dependencies install

---
+++ backend/src/common/helpers/handle-error.helper.js
@@ -0,0 +1,25 @@
+import { responseError } from "./function.helper.js";
+import jwt from "jsonwebtoken";
+import { statusCodes } from "./status-code.helper.js";
+
+/**
+ * Để ở cuối để gom tất cả các lỗi có trong dự án
+ */
+export const appErorr = (err, req, res, next) => {
+    console.log(`Middleware đặc biệt, bắt lỗi`, err);
+
+    if (err instanceof jwt.JsonWebTokenError) {
+        err.code = statusCodes.UNAUTHORIZED; // 401 => FE logout người dùng
+        err.message = "Invalid Token";
+    }
+    if (err instanceof jwt.TokenExpiredError) {
+        err.code = statusCodes.FORBIDDEN; // 403 => FE sẽ gọi api POST: api/auth/refresh-token
+        err.message = "Token Expired";
+    }
+
+    // undefinde: không tồn tại
+    // null: có tồn tại nhưng bị rỗng
+    const response = responseError(err?.message, err.code);
+
+    res.status(response.statusCode).json(response);
+};
