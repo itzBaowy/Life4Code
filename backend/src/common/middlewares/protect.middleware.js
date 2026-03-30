1edf7191a7131004720b81195a10e07f5443a23e
itBaowy <notbao.js@gmail.com>
2026-03-31 02:05:55 +0700

    protect middleware for checking auth

---
+++ backend/src/common/middlewares/protect.middleware.js
@@ -0,0 +1,51 @@
+import { tokenService } from "../../services/token.service.js";
+import { UnauthorizedException } from "../helpers/exception.helper.js";
+import prisma from "../../prisma/connect.prisma.js";
+
+export const protect = async (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+        throw new UnauthorizedException("No authorization");
+    }
+
+    const [type, token] = authorization.split(" ");
+    if (type !== "Bearer") {
+        throw new UnauthorizedException("Token is not Bearer");
+    }
+    if (!token) {
+        throw new UnauthorizedException("No token");
+    }
+
+    // // Kiểm tra token có trong blacklist không
+    // const blacklistedToken = await prisma.blackListToken.findFirst({
+    //     where: {
+    //         token: token,
+    //     },
+    // });
+    // if (blacklistedToken) {
+    //     throw new UnauthorizedException("Token has been revoked");
+    // }
+
+    const { userId } = tokenService.verifyAccessToken(token);
+
+    const userExist = await prisma.users.findUnique({
+        where: {
+            id: userId,
+        },
+        include: {
+            role: true,
+        },
+    });
+    if (!userExist) {
+        throw new UnauthorizedException("Cannot find user");
+    }
+
+    // // Check if user account is active
+    // if (!userExits.isActive || userExits.status === 'INACTIVE') {
+    //     throw new UnauthorizedException("Your account has been deactivated. Please contact the administrator.");
+    // }
+
+    req.user = userExist
+
+    next();
+};
